1015b90258a9e34663d2ccdffb4861b3e9a3c780
AndryBethpalko <sprint4ua@gmail.com>
2022-09-19 16:58:31 +0300

    Add event listener

--- src/slate.js
+++ src/slate.js
@@ -2,6 +2,7 @@
 import React, {useState} from 'react'
 // Import the Slate editor factory.
 import {createEditor} from 'slate'
+import './slate.css'
 
 // Import the Slate components and React plugin.
 import {Slate, Editable, withReact} from 'slate-react'
@@ -15,11 +16,23 @@ const initialValue = [
 
 function SlateEditor() {
   // Create a Slate editor object that won't change across renders.
-  const [editor] = useState(() => withReact(createEditor()))
+  const [editor] = useState(() => withReact(createEditor()));
+
   return (
-    <Slate editor={editor} value={initialValue}>
-      <Editable/>
-    </Slate>
+    <div className='editor'>
+      <Slate editor={editor} value={initialValue}>
+        <Editable
+          onKeyDown={event => {
+            if (event.key === '&') {
+              // Prevent the ampersand character from being inserted.
+              event.preventDefault()
+              // Execute the `insertText` method when the event occurs.
+              editor.insertText('and')
+            }
+          }}
+        />
+      </Slate>
+    </div>
   )
 }
 
